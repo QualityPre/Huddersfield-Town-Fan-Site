23f98b78901ee8c237651a9cc882c99bdffab560
QualityPre <qualitypre@protonmail.com>
2022-03-25 15:48:38 +0000

    added styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import 'normalize.css';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
+
 export default function Layout({ children }) {
     return (
-        <div>
+        <>
+            <GlobalStyles />
+            <Typography />
             <Navbar />
             {children}
 
             <Footer />
-        </div>
+        </>
     );
 }
