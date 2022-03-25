ccb5c32d2e130e735cd302dfd56c3b6ebdc01260
QualityPre <qualitypre@protonmail.com>
2022-03-25 16:07:02 +0000

    removed styled components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,14 +3,9 @@ import 'normalize.css';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
-import GlobalStyles from '../styles/GlobalStyles';
-import Typography from '../styles/Typography';
-
 export default function Layout({ children }) {
     return (
         <>
-            <GlobalStyles />
-            <Typography />
             <Navbar />
             {children}
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,8 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import styled from 'styled-components';
-
-const NavbarStyles = styled.nav`
-    margin-bottom: 3rem;
-    ul {
-        margin: 0.2rem 0.1rem 0 0;
-        padding: 0.8rem;
-        text-align: center;
-        list-style: none;
-        display: grid;
-        grid-template-columns: 0.2fr auto 0.2fr;
-        grid-gap: 2rem;
-        align-items: center;
-    }
-
-    a {
-        font-size: 3rem;
-        text-decoration: none;
-        &:hover {
-            color: var(--red);
-        }
-    }
-`;
 
 const Navbar = () => (
-    <NavbarStyles>
+    <nav>
         <ul>
             <li>
                 <Link to="/">Home</Link>
@@ -37,7 +14,7 @@ const Navbar = () => (
                 <Link to="/about">About</Link>
             </li>
         </ul>
-    </NavbarStyles>
+    </nav>
 );
 
 export default Navbar;
