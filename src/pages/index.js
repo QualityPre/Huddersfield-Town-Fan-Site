c5642827c4beea4664690dda94743960bd3a3c76
QualityPre <qualitypre@protonmail.com>
2022-03-25 16:19:01 +0000

    tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function IndexPage() {
-    return <>Hello this is index Page</>;
+    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
